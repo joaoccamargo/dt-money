dfb261fa681ca5516198b9f5e04b3b9b2ced2c93
João Camargo <jclaudio00_12@hotmail.com>
2025-03-17 20:18:20 -0300

    added table and icons, others adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Transactions } from './pages/Transactions'
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Teste</h1>
-      
+      <Transactions />      
     </ThemeProvider>
   )
 }
