fb202b7b92e8a6e24ee25525b87fc6acd4425d8b
João Camargo <jclaudio00_12@hotmail.com>
2025-03-25 19:48:00 -0300

    created context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,33 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
-interface Transaction {
-  id: number;
-  description: string;
-  type: 'income' | 'outcome'
-  price: number;
-  category: string;
-  createdAt: string;
-}
+
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransactions() {
-    const res = await fetch('http://localhost:3333/transactions')
-    const data = await res.json()
-    setTransactions(data);
-    console.log(data)
-  }
-
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
-
+  const { transactions } = useContext(TransactionsContext);
+  
 
   return (
     <div>
@@ -43,7 +25,7 @@ export function Transactions() {
                   <td width={'50%'}>{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      + R$ {transaction.price}
+                      {transaction.price}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
